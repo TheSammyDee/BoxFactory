0efabf5ddae1bc6c130127e76b4963b204453ddd
Samantha Derochie <samantha.derochie@gmail.com>
2018-04-01 17:45:28 -0700

    added box cam rotation via touch

---
+++ Assets/Scripts/BoxCamRotator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxCamRotator : MonoBehaviour
+{
+    private float maxRotY = 155;
+    private float maxRotX = 50;
+
+    private float rotX = 0;
+    private float rotY = -20;
+    private Vector3 defaultRot;
+
+    private void Start()
+    {
+        defaultRot = new Vector3(0, rotY, 0);
+    }
+
+    public void RotateCam(Vector2 delta)
+    {
+        rotY += delta.x;
+        rotX += delta.y;
+
+        if (Mathf.Abs(rotY) > maxRotY)
+        {
+            rotY = rotY > 0 ? maxRotY : -maxRotY;
+        }
+        if (Mathf.Abs(rotX) > maxRotX)
+        {
+            rotX = rotX > 0 ? maxRotX : -maxRotX;
+        }
+
+        transform.rotation = Quaternion.identity;
+        transform.Rotate(Vector3.up, rotY);
+        transform.Rotate(Vector3.right, rotX);
+    }
+
+    public void Reset()
+    {
+        transform.rotation = Quaternion.Euler(defaultRot);
+    }
+}
--- Assets/Scripts/BoxViewer.cs
+++ Assets/Scripts/BoxViewer.cs
@@ -2,14 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoxViewer : MonoBehaviour {
+public class BoxViewer : MonoBehaviour
+{
+    public BoxCamRotator boxCamRotator;
 
-    public Camera viewer;
+    private float rotationRatioX = 0.5f;
+    private float rotationRatioY = 0.1f;
 
     private bool moving;
+    private Vector3 lastPos;
+    
 
     public void Update()
     {
         
+    }
+    
+    public void OnDragBegin()
+    {
+        lastPos = Input.mousePosition;
+    }
+
+    public void OnDrag()
+    {
+        Vector2 delta = Input.mousePosition - lastPos;
+        delta.y = -delta.y;
+        boxCamRotator.RotateCam(new Vector2(delta.x * rotationRatioX, delta.y * rotationRatioY));
+        lastPos = Input.mousePosition;
+    }
+
+    public void OnDragEnd()
+    {
+
     }
 }
