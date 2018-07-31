7bff59b331e7a5edd64ed1fafc485122649a3298
Samantha Derochie <samantha.derochie@gmail.com>
2018-07-30 17:56:31 -0700

    created and tested interactive 3D push button

---
+++ Assets/Scripts/Editor/ButtonPush3DEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+
+[CustomEditor(typeof(ButtonPush3D))]
+public class ButtonPush3DEditor : ButtonEditor
+{
+    public override void OnInspectorGUI()
+    {
+        ButtonPush3D target3DButton = (ButtonPush3D)target;
+
+        target3DButton.textOffset = EditorGUILayout.IntField("Text offset", target3DButton.textOffset);
+
+        base.OnInspectorGUI();
+    }
+}
---
+++ Assets/Scripts/UI/ButtonPush3D.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonPush3D : Button
+{
+    public int textOffset;
+
+    private Text text;
+    private Vector3 upPosition;
+    private Vector3 downPosition;
+    private bool pressing;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        text = GetComponentInChildren<Text>();
+        upPosition = text.transform.position;
+        downPosition = upPosition - new Vector3(0, textOffset, 0);
+        pressing = false;
+    }
+
+    protected void Update()
+    {
+        if (IsPressed() && !pressing)
+        {
+            MoveTextDown();
+            pressing = true;
+        }
+        else if (!IsPressed() && pressing)
+        {
+            MoveTextUp();
+            pressing = false;
+        }
+    }
+
+    private void MoveTextDown()
+    {
+        text.transform.position = downPosition;
+    }
+
+    private void MoveTextUp()
+    {
+        text.transform.position = upPosition;
+    }
+}
