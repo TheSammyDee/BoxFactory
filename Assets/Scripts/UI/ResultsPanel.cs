596a7c9f17e70e5dac6ceb382807e2e59e55adab
Samantha Derochie <samantha.derochie@gmail.com>
2018-08-01 08:21:38 -0700

    created and added ticker tape UI asset

--- Assets/Scripts/UI/ResultsPanel.cs
+++ Assets/Scripts/UI/ResultsPanel.cs
@@ -12,6 +12,12 @@ public class ResultsPanel : MonoBehaviour {
     Text resultText;
 
     [SerializeField]
+    Image longTextBG;
+
+    [SerializeField]
+    Image shortTextBG;
+
+    [SerializeField]
     Button resultButton;
 
     [SerializeField]
@@ -25,14 +31,18 @@ public class ResultsPanel : MonoBehaviour {
 
         if (solved)
         {
-            result = "Correct";
+            result = "Correct!" + "\nYour score: " + playerScore + "\nPerfect: " + perfectScore;
+            longTextBG.gameObject.SetActive(true);
+            shortTextBG.gameObject.SetActive(false);
         }
         else
         {
-            result = "Nope";
+            result = "Not quite...";
+            longTextBG.gameObject.SetActive(false);
+            shortTextBG.gameObject.SetActive(true);
         }
 
-        resultText.text = result + "\nPerfect score: " + perfectScore + "\nYour score: " + playerScore;
+        resultText.text = result;
     }
 
     public void ShowResult()
