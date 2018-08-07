8b8ca8794d18abedd62864ed18a48ec9e41eea4e
Samantha Derochie <samantha.derochie@gmail.com>
2018-08-07 07:59:13 -0700

    in the middle of working on new command scroll bar

---
+++ Assets/ImageScroller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageScroller : MonoBehaviour{
+
+    [SerializeField]
+    RectTransform content;
+
+    private List<Image> images;
+    private int spacing = 16;
+
+    private void Start()
+    {
+        images = new List<Image>();
+    }
+
+    public void AddImage(Image image)
+    {
+        Vector3 pos;
+        float xPos;
+        if (images.Count == 0)
+        {
+            pos = content.transform.position;
+            xPos = -(content.rect.width / 2f) + (image.rectTransform.rect.width / 2f) + spacing;
+            pos.x = xPos;
+            image.transform.SetParent(content);
+            image.transform.position = pos;
+        }
+        else
+        {
+            Image lastImage = images[images.Count - 1];
+            pos = lastImage.transform.position;
+        }
+    }
+}
